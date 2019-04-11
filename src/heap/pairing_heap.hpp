d5af2552b139ea65884caa2384b0c6daf67724fe
SchrodingerZhu <118010469@link.cuhk.edu.cn>
2019-04-11 20:37:39 +0800

    re-start paring heap

--- src/heap/pairing_heap.hpp
+++ src/heap/pairing_heap.hpp
@@ -6,6 +6,12 @@
 #ifndef DATA_STRUCTURE_FOR_LOVE_PAIRING_HEAP_HPP
 #define DATA_STRUCTURE_FOR_LOVE_PAIRING_HEAP_HPP
 namespace data_structure {
+    template<typename T>
+    struct ParingHeapNode {
+    };
 
+    template<typename T, typename Compare>
+    class PairingHeap : Heap<T, Compare> {
+    };
 }
 #endif //DATA_STRUCTURE_FOR_LOVE_PAIRING_HEAP_HPP
