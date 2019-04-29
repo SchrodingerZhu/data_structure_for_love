d4cc7f27a98347a7e58285e0671810251687fbd7
SchrodingerZhu <118010469@link.cuhk.edu.cn>
2019-04-29 23:20:01 +0800

    start yfast_treap

---
+++ utils/yfast_treap.hpp
@@ -0,0 +1,12 @@
+//
+// Created by schrodinger on 19-4-29.
+//
+
+#ifndef DATA_STRUCTURE_FOR_LOVE_YFAST_TREAP_HPP
+#define DATA_STRUCTURE_FOR_LOVE_YFAST_TREAP_HPP
+namespace data_structure::utils {
+    struct TNode {
+
+    };
+}
+#endif //DATA_STRUCTURE_FOR_LOVE_YFAST_TREAP_HPP
