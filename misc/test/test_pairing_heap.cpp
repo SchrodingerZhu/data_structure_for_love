f427026581c5fef1bf21aa06c559a90396308e3c
SchrodingerZhu <118010469@link.cuhk.edu.cn>
2019-05-06 21:47:26 +0800

    add paring heap proto

--- misc/test/test_pairing_heap.cpp
+++ misc/test/test_pairing_heap.cpp
@@ -1,9 +1,86 @@
 //
-// Created by schrodinger on 19-4-10.
+// Created by schrodinger on 19-4-9.
 //
-
 #include <pairing_heap.hpp>
+#include <iostream>
+#include <vector>
+#include <random>
+#include <cassert>
 
-int main(){
+std::mt19937_64 eng{};
+std::uniform_int_distribution<int> dist(0, std::numeric_limits<int>::max());
+#define RANGE 100000
+#define get_rand() dist(eng)
 
+inline auto random_string() {
+    std::string test{};
+    auto len = get_rand() % 233;
+    while (len--) {
+        test.push_back(static_cast<char>(get_rand() % 128));
+    }
+    return test;
+}
+
+signed main() {
+    using namespace data_structure;
+    PairingHeap<std::string> test;
+    PairingHeap<std::string, std::greater<>> test2;
+    std::vector<std::string> temp, temp2, res, res2;
+    for (auto i = 0; i < RANGE; ++i) {
+        auto m = random_string();
+        test.push(m);
+        temp.push_back(m);
+        m = random_string();
+        temp2.push_back(m);
+        test2.emplace(std::move(m));
+    }
+    while (test.size()) {
+        res.push_back(test.top());
+        test.pop();
+    }
+    while (test2.size()) {
+        res2.push_back(test2.top());
+        test2.pop();
+    }
+    std::sort(temp.begin(), temp.end());
+    std::sort(temp2.begin(), temp2.end(), std::greater<std::string>{});
+    assert(temp == res);
+    assert(temp2 == res2);
+    PairingHeap<int> to_copy = {9, 6, 5, 2, 1, 0, -1, 2, 3}, copy_res;
+    copy_res = to_copy;
+    std::vector<int> a = {9, 6, 5, 2, 1, 0, -1, 2, 3}, b;
+    while (copy_res.size()) {
+        b.push_back(copy_res.top());
+        copy_res.pop();
+    }
+    std::sort(a.begin(), a.end());
+    assert(a == b);
+
+    PairingHeap<int> to_move = {9, 6, 5, 2, 1, 0, -1, 2, 3}, move_res;
+    copy_res = std::move(to_move);
+    std::vector<int> d;
+    while (copy_res.size()) {
+        d.push_back(copy_res.top());
+        copy_res.pop();
+    }
+    assert(a == d);
+
+    PairingHeap<int> decrease1 = {1, 3, 5, 1, 2}, decrease2 = {1, 3, 5, 1, 2};
+    auto ha = decrease1.emplace_and_hold(9);
+    ha.re_emplace(-1);
+    std::vector<int> res_ha = {-1, 1, 1, 2, 3, 5}, test_ha;
+    while (decrease1.size()) {
+        test_ha.push_back(decrease1.top());
+        decrease1.pop();
+    }
+    assert(res_ha == test_ha);
+    auto hb = decrease2.push_and_hold(9);
+    hb.re_push(4);
+    hb.re_emplace(-2);
+    std::vector<int> res_hb = {-2, 1, 1, 2, 3, 5}, test_hb;
+    while (decrease2.size()) {
+        test_hb.push_back(decrease2.top());
+        decrease2.pop();
+    }
+    assert(res_hb == test_hb);
 }
--- src/heap/pairing_heap.hpp
+++ src/heap/pairing_heap.hpp
@@ -6,6 +6,234 @@
 #ifndef DATA_STRUCTURE_FOR_LOVE_PAIRING_HEAP_HPP
 #define DATA_STRUCTURE_FOR_LOVE_PAIRING_HEAP_HPP
 namespace data_structure {
+    template<typename T>
+    struct PairingHeapNode {
+        PairingHeapNode *next{}, *parent{}, *children{};
+        T value;
 
+        explicit PairingHeapNode(const T &t) : value(t) {}
+
+        template<typename ...Args>
+        explicit PairingHeapNode(Args &&...args): value(std::forward<Args>(args)...) {};
+    };
+
+    template<typename T,
+            typename Compare = std::less<T>>
+    class PairingHeap : public Heap<T, Compare> {
+        using Node = PairingHeapNode<T>;
+
+        constexpr static Compare compare{};
+
+        static void destroy(Node *h) noexcept {
+            Node *t = nullptr;
+            if (h) {
+                while ((t = h->children)) {
+                    h->children = h->children->next;
+                    destroy(t);
+                }
+            }
+            delete h;
+        }
+
+        static Node *copy(Node *node, Node *parent = nullptr) {
+            if (!node)
+                return nullptr;
+            auto p = new Node(node->value);
+            p->parent = parent;
+            if (node->children) p->children = copy(node->children, p);
+            if (node->next) p->next = copy(node->next);
+            return p;
+        }
+
+        static Node *push_front(Node *list, Node *x) noexcept {
+            x->next = list;
+            return x;
+        }
+
+        static Node *remove_node(Node *list, Node *x) noexcept {
+            auto p = list;
+            if (x == list) return x->next;
+            while (p && p->next != x) p = p->next;
+            if (p) p->next = x->next;
+            return list;
+        }
+
+        static Node *merge(Node *h1, Node *h2) noexcept {
+            if (!h1) return h2;
+            if (!h2) return h1;
+            if (compare(h2->value, h1->value)) std::swap(h1, h2);
+            h2->next = h1->children;
+            h1->children = h2;
+            h2->parent = h1;
+            h1->next = nullptr;
+            return h1;
+        }
+
+        static Node *push(Node *h, const T &x) noexcept {
+            return merge(h, new Node(x));
+        }
+
+        template<typename ...Args>
+        static Node *emplace(Node *h, Args &&...args) noexcept {
+            return merge(h, new Node(std::forward<Args>(args)...));
+        }
+
+        static const T &top(Node *h) noexcept {
+            return h->value;
+        }
+
+
+        static Node *push_decreased_key(Node *h, Node *x, const T &key) noexcept {
+            x->value = key;
+            if (x->parent) {
+                x->parent->children = remove_node(x->parent->children, x);
+                x->parent = nullptr;
+                return merge(h, x);
+            }
+            return h;
+        }
+
+        template<typename ...Args>
+        static Node *emplace_decreased_key(Node *h, Node *x, Args &&...args) noexcept {
+            x->value = T(std::forward<Args>(args)...);
+            if (x->parent) {
+                x->parent->children = remove_node(x->parent->children, x);
+                x->parent = nullptr;
+                return merge(h, x);
+            }
+            return h;
+        }
+
+        static Node *pop(Node *h) noexcept {
+            Node *x{}, *y{}, *list{};
+            while ((x = h->children)) {
+                if ((h->children = y = x->next))
+                    h->children = h->children->next;
+                list = push_front(list, merge(x, y));
+            }
+            x = nullptr;
+            while ((y = list)) {
+                list = list->next;
+                x = merge(x, y);
+            }
+            delete h;
+            return x;
+        }
+
+        Node *root = nullptr;
+        size_t _size = 0;
+    public:
+
+        class value_holder {
+            Node *node;
+            PairingHeap *heap;
+        public:
+            value_holder(Node *node, PairingHeap *heap) : node(node), heap(heap) {}
+
+            const auto &get() {
+                return node->value;
+            }
+
+            void re_push(const T &value) {
+                heap->root = push_decreased_key(heap->root, node, value);
+            }
+
+            template<typename ...Args>
+            void re_emplace(Args &&... args) {
+                heap->root = emplace_decreased_key(heap->root, node, std::forward<Args>(args)...);
+            }
+        };
+
+        PairingHeap() = default;
+
+        PairingHeap(const std::initializer_list<T> &list) {
+            for (const auto &i : list) {
+                push(i);
+            }
+        }
+
+        PairingHeap(PairingHeap &&that) noexcept {
+            this->root = that.root;
+            this->_size = that._size;
+            that.root = nullptr;
+            that._size = 0;
+        }
+
+        PairingHeap &operator=(PairingHeap &&that) noexcept {
+            this->~PairingHeap();
+            this->root = that.root;
+            this->_size = that._size;
+            that.root = nullptr;
+            that._size = 0;
+            return *this;
+        }
+
+        PairingHeap(const PairingHeap &that) {
+            this->root = copy(that.root);
+            this->_size = that._size;
+        }
+
+        PairingHeap &operator=(const PairingHeap &that) {
+            this->~PairingHeap();
+            this->root = copy(that.root);
+            this->_size = that._size;
+            return *this;
+        };
+
+
+        ~PairingHeap() {
+            destroy(root);
+        }
+
+        const T &top() override {
+            return top(root);
+        }
+
+        void push(const T &t) override {
+            root = push(root, t);
+            _size += 1;
+        }
+
+        template<typename ...Args>
+        void emplace(Args &&... args) noexcept {
+            root = emplace(root, std::forward<Args>(args)...);
+            _size += 1;
+        }
+
+        void merge(PairingHeap &that) {
+            root = merge(root, that.root);
+            that.root = nullptr;
+            _size += that._size;
+            that._size = 0;
+        }
+
+        size_t size() const override {
+            return _size;
+        };
+
+        void pop() override {
+            root = pop(root);
+            _size -= 1;
+        }
+
+        bool is_empty() { return root == nullptr; }
+
+        value_holder push_and_hold(const T &t) {
+            auto m = new Node(t);
+            root = merge(root, m);
+            _size += 1;
+            return {m, this};
+        }
+
+        template<typename ...Args>
+        value_holder emplace_and_hold(Args &&...args) {
+            auto m = new Node(std::forward<Args>(args)...);
+            root = merge(root, m);
+            _size += 1;
+            return {m, this};
+        }
+
+
+    };
 }
 #endif //DATA_STRUCTURE_FOR_LOVE_PAIRING_HEAP_HPP
